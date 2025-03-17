b53868de1c47795f69bd6221dd8d311e62b97a87
anitach2003 <123134603+anitach2003@users.noreply.github.com>
2025-03-17 10:45:37 +0330

    Update conv.py
    Signed-off-by: anitach2003 <123134603+anitach2003@users.noreply.github.com>

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -90,108 +90,81 @@ class Conv(nn.Module):
             (torch.Tensor): Output tensor.
         """
         return self.act(self.conv(x))
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+import torch
+import torch.nn as nn
+import math
+import torch.nn.functional as F
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
 class ShuffleAttention(nn.Module):
+    def __init__(self, channel=512 ,  reduction=16):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
 
-    def __init__(self, channel=512, reduction=16, G=8):
-        super().__init__()
-        self.G = G
-        self.channel = channel
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.gn = nn.GroupNorm(channel // (2 * G), channel // (2 * G))
-        self.cweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
-        self.cbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
-        self.sweight = Parameter(torch.zeros(1, channel // (2 * G), 1, 1))
-        self.sbias = Parameter(torch.ones(1, channel // (2 * G), 1, 1))
-        self.sigmoid = nn.Sigmoid()
+        mip = max(8, channel // reduction)
 
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-    @staticmethod
-    def channel_shuffle(x, groups):
-        b, c, h, w = x.shape
-        x = x.reshape(b, groups, -1, h, w)
-        x = x.permute(0, 2, 1, 3, 4)
-
-        # flatten
-        x = x.reshape(b, -1, h, w)
-
-        return x
+        self.conv1 = nn.Conv2d(channel, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+        
+        self.conv_h = nn.Conv2d(mip, 512, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, 512, kernel_size=1, stride=1, padding=0)
+        
 
     def forward(self, x):
-        b, c, h, w = x.size()
-        # group into subfeatures
-        x = x.view(b * self.G, -1, h, w)  # bs*G,c//G,h,w
+        identity = x
+        
+        n,c,h,w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
 
-        # channel_split
-        x_0, x_1 = x.chunk(2, dim=1)  # bs*G,c//(2*G),h,w
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y) 
+        
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
 
-        # channel attention
-        x_channel = self.avg_pool(x_0)  # bs*G,c//(2*G),1,1
-        x_channel = self.cweight * x_channel + self.cbias  # bs*G,c//(2*G),1,1
-        x_channel = x_0 * self.sigmoid(x_channel)
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
 
-        # spatial attention
-        x_spatial = self.gn(x_1)  # bs*G,c//(2*G),h,w
-        x_spatial = self.sweight * x_spatial + self.sbias  # bs*G,c//(2*G),h,w
-        x_spatial = x_1 * self.sigmoid(x_spatial)  # bs*G,c//(2*G),h,w
+        out = identity * a_w * a_h
 
-        # concatenate along channel axis
-        out = torch.cat([x_channel, x_spatial], dim=1)  # bs*G,c//G,h,w
-        out = out.contiguous().view(b, -1, h, w)
-
-        # channel shuffle
-        out = self.channel_shuffle(out, 2)
         return out
-class ResBlock_CBAM(nn.Module):
-    def __init__(self, in_places, places, stride=1, downsampling=False, expansion=1):
-        super(ResBlock_CBAM, self).__init__()
-        self.expansion = expansion
-        self.downsampling = downsampling
 
-        self.bottleneck = nn.Sequential(
-            nn.Conv2d(in_channels=in_places, out_channels=places, kernel_size=1, stride=1, bias=False),
-            nn.BatchNorm2d(places),
-            nn.LeakyReLU(0.1, inplace=True),
-            nn.Conv2d(in_channels=places, out_channels=places, kernel_size=3, stride=stride, padding=1, bias=False),
-            nn.BatchNorm2d(places),
-            nn.LeakyReLU(0.1, inplace=True),
-            nn.Conv2d(in_channels=places, out_channels=places * self.expansion, kernel_size=1, stride=1,
-                      bias=False),
-            nn.BatchNorm2d(places * self.expansion),
-        )
-        # self.cbam = CBAM(c1=places * self.expansion, c2=places * self.expansion, )
-        self.cbam = CBAM(c1=places * self.expansion)
-
-        if self.downsampling:
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels=in_places, out_channels=places * self.expansion, kernel_size=1, stride=stride,
-                          bias=False),
-                nn.BatchNorm2d(places * self.expansion)
-            )
-        self.relu = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        residual = x
-        out = self.bottleneck(x)
-        out = self.cbam(out)
-        if self.downsampling:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-        return out
 class Conv2(Conv):
     """
     Simplified RepConv module with Conv fusing.
